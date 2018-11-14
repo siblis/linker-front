aaa4008d7448caa42202ae149d59080a38b955dd
Mark Vaisman <mark.v.vaisman@gmail.com>
2018-11-15 00:07:55 +1100

    Верстка страницы авторизации/регистрации - v3.0

--- src/components/Enter/Enter.jsx
+++ src/components/Enter/Enter.jsx
@@ -13,8 +13,14 @@ export default class Enter extends PureComponent {
             txtPassword: '',
             txtButtonEnter: '',
             txtButtonRegistration: '',
+            txtInvalidEmail: '',
+            txtEmptyPassword: '',
             errorMessageEnter: '',
-            errorMessageRegistration: ''
+            errorMessageRegistration: '',
+            enterEmail: '',
+            enterPassword: '',
+            registrationEmail: '',
+            registrationPassword: ''
         };
     }
     
@@ -25,7 +31,57 @@ export default class Enter extends PureComponent {
             .then(data => this.setState(data));
     }
     
+    handleInputChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+    
+    handleButtonClick = (event) => {
+        switch (event.target.name) {
+            case 'enter':
+                this.authorization(this.state.enterEmail, this.state.enterPassword);
+                break;
+            case 'registration':
+                this.registration(this.state.registrationEmail, this.state.registrationPassword);
+                break;
+        }
+    };
+    
+    validateEmailPassword = (email, password) => {
+        if (!/([a-zA-Z0-9-_\\.]+)(@)([a-zA-Z0-9-_\\.]+)\.([a-z]{2,})/.test(email)) {
+            return this.state.txtInvalidEmail;
+        }
+        if (!password.trim().length) {
+            return this.state.txtEmptyPassword;
+        }
+        return '';
+    };
+    
+    authorization = (email, password) => {
+        const errors = this.validateEmailPassword(email,password);
+        if (errors === '') {
+            // Запрос авторизации
+        } else {
+            this.setState({
+                errorMessageEnter: errors
+            });
+        }
+    };
+    
+    registration = (email, password) => {
+        const errors = this.validateEmailPassword(email,password);
+        if (errors === '') {
+            // Запрос регистрации
+        } else {
+            this.setState({
+                errorMessageRegistration: errors
+            });
+        }
+    };
+    
     render() {
+        const {enterEmail, enterPassword, registrationEmail, registrationPassword} = this.state;
         return (
             <div>
                 <div className="enter-section">
@@ -33,22 +89,42 @@ export default class Enter extends PureComponent {
                         {this.state.txtEnter}
                     </div>
                     <div className="input-blocks">
-                        <input type="text" placeholder={this.state.txtUserName}/>
-                        <input type="password" placeholder={this.state.txtPassword}/>
+                        <input name="enterEmail"
+                               type="text"
+                               value={enterEmail}
+                               onChange={this.handleInputChange}
+                               placeholder={this.state.txtUserName}/>
+                        <input name="enterPassword"
+                               type="password"
+                               value={enterPassword}
+                               onChange={this.handleInputChange}
+                               placeholder={this.state.txtPassword}/>
                         <div className="error-message">{this.state.errorMessageEnter}</div>
                     </div>
-                    <button>{this.state.txtButtonEnter}</button>
+                    <button name="enter" onClick={this.handleButtonClick}>
+                        {this.state.txtButtonEnter}
+                    </button>
                 </div>
                 <div className="registration-section">
                     <div className="title-block">
                         {this.state.txtRegistration}
                     </div>
                     <div className="input-blocks">
-                        <input type="text" placeholder={this.state.txtEmail}/>
-                        <input type="text" placeholder={this.state.txtPassword}/>
+                        <input name="registrationEmail"
+                               type="text"
+                               value={registrationEmail}
+                               onChange={this.handleInputChange}
+                               placeholder={this.state.txtEmail}/>
+                        <input name="registrationPassword"
+                               type="text"
+                               value={registrationPassword}
+                               onChange={this.handleInputChange}
+                               placeholder={this.state.txtPassword}/>
                         <div className="error-message">{this.state.errorMessageRegistration}</div>
                     </div>
-                    <button>{this.state.txtButtonRegistration}</button>
+                    <button name="registration" onClick={this.handleButtonClick}>
+                        {this.state.txtButtonRegistration}
+                    </button>
                 </div>
             </div>
         );
