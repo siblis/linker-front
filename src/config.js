3e551dad4bf357d1703c6862f5ee3d3bd01f5c84
Mark Vaisman <mark.v.vaisman@gmail.com>
2018-12-04 20:20:59 +1100

    Добавлен сервер для создания скриншотов сайтов

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     api: 'https://api.linker.gb-tools.ru/api/v1',
-    thumbs: 'https://api.letsvalidate.com/v1/thumbs?size=og&url=',
+    thumbs: 'https://api.linker.gb-tools.ru/api/v1/thumbs/?url=',
     collectionBaseUrl: 'https://linker.gb-tools.ru/collection/'
 }
